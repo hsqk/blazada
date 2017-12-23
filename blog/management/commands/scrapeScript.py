c7cb7e00038f76825691e5f5cd7f6ac8d141149c
Kenneth Huang <hsqkenneth@gmail.com>
2017-12-23 19:17:07 +0800

    linked up to telegram bot, updates chatId in trackers etc. it basically works

--- blog/management/commands/scrapeScript.py
+++ blog/management/commands/scrapeScript.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand, CommandError
-from blog.models import Trackee
+from blog.models import Trackee, Tracker
 
 import requests
 import bs4
@@ -7,10 +7,6 @@ import os
 
 
 import telegram
-token = '458733632:AAHzH3NtBMk2fvv4P0zb3Yr0qVUjnawew7k'
-bot = telegram.Bot(token=token)
-def botSend(chatId, text):
-    
 #"364600088"
 
 
@@ -20,23 +16,29 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         pass
         #parser.add_argument('poll_id', nargs='+', type=int)
-
+    
     def handle(self, *args, **options):
+        token = '458733632:AAHzH3NtBMk2fvv4P0zb3Yr0qVUjnawew7k'
+        bot = telegram.Bot(token=token)
         allTrackees = Trackee.objects.all()
         for i in range(len(allTrackees)):
             trackee = allTrackees[i]
             try:
-                response = requests.get(url)
+                response = requests.get(trackee.url)
                 soup = bs4.BeautifulSoup(response.text, "html.parser")
                 priceTag = soup.find("span", { "class" : "price" } )
                 price = float(priceTag.text[4:])
                 if price < float(trackee.target):
                     self.stdout.write(trackee.url + " under target")
+                    chatId = Tracker.objects.get(mobile=trackee.mobile).chatId
+                    print(chatId)
+                    text = "Yo! " + trackee.name + " has reached the target price of " + str(trackee.target)
                     bot.send_message(chat_id=chatId, text=text)
                     trackee.delete()
                 else:
                     self.stdout.write(trackee.url + " not under target")
-            except:
+            except Exception as error:
+                self.stdout.write(repr(error))
                 self.stdout.write(trackee.url + " not valid")
                 trackee.delete()
 
--- stayingAliveBot.py
+++ stayingAliveBot.py
@@ -1,11 +1,19 @@
 token = '458733632:AAHzH3NtBMk2fvv4P0zb3Yr0qVUjnawew7k'
+
 import telegram
-bot = telegram.Bot(token=token)
+import logging
 
 from telegram.ext import Updater
+from telegram.ext import CommandHandler
+from telegram.ext import MessageHandler, Filters
+
+from blog.models import Trackee, Tracker
+
+bot = telegram.Bot(token=token)
+
+
 updater = Updater(token=token)
 dispatcher = updater.dispatcher
-import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
 
 def start(bot, update):
@@ -19,7 +27,8 @@ def start(bot, update):
     reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard, one_time_keyboard=True,)
     bot.send_message(chat_id=update.message.chat_id, text="Custom Keyboard Test", reply_markup=reply_markup)
 
-from telegram.ext import CommandHandler
+
+
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
@@ -27,8 +36,6 @@ dispatcher.add_handler(start_handler)
 updater.start_polling()
 
 
-
-from telegram.ext import MessageHandler, Filters
 
 def echo(bot, update):
     if update.message.text=='1':
@@ -38,7 +45,11 @@ def echo(bot, update):
 
 def matchMobile(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text=update.message.contact.phone_number)
-    #link chatid to mobile number in trackers model
+    chatId = update.message.chat_id
+    mobile = update.message.contact.phone_number
+    tracker = Tracker.objects.get(mobile = "+"+mobile)
+    tracker.chatId = chatId
+    tracker.save()
 
 echo_handler = MessageHandler(Filters.text, echo)
 dispatcher.add_handler(echo_handler)
@@ -49,21 +60,7 @@ dispatcher.add_handler(contact_handler)
 
 # def check: get chatid, lookup mobile number in django trackee table, get everything, parse and send
 
-
-
-
-
-
-
-
-
-
-
-
-updater.stop()
-
-
-bot.send_message(chat_id="364600088", text="I'm a bot, please talk to me!")
+#updater.stop()
 
 
 
