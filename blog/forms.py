5f9cab1ac2cde58d3fd2e494e6cc7fc870d0be13
Kenneth Huang <hsqkenneth@gmail.com>
2017-12-24 19:06:23 +0800

    prettify price tracking form

--- blog/forms.py
+++ blog/forms.py
@@ -9,7 +9,11 @@ class TrackeeForm(forms.ModelForm):
     class Meta:
         model = Trackee
         fields = ('url', 'target', 'mobile')
-    
+        widgets = {
+            'url': forms.URLInput(attrs={'placeholder': 'e.g. https://www.lazada.sg/xiaomi-redmi-4x-3gb-32gb-gold-20499139.html?spm=a2o42.prod.0.0.18feea70Ul1BP0', 'title':'Copy and paste from product page'}),
+            'target': forms.NumberInput(attrs={'placeholder': 'e.g. 180', 'title': 'Exclude $ sign'}),
+            'mobile': forms.TextInput(attrs={'placeholder': 'e.g. +65 96987636', 'title': 'Include + sign, country code'})
+            }
 #    captcha = ReCaptchaField()
 
 
